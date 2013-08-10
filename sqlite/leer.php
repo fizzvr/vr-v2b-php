88ed0ba7692b9517a50f5e34b38d6462e91b42d4
fizzvr <fizzvr@gmail.com>
2013-08-10 00:10:30 -0500

    lectura de datos desde SQLite

--- sqlite/leer.php
+++ sqlite/leer.php
@@ -1,4 +1,19 @@
-<?php
-	$bd=new SQLite3("preguntasyrespuestas.db") ;
-	$bd->close();
-?>
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>SQLite 3</title>
+</head>
+<body style="font-size: 32px;">
+	<table border=10>
+		<?php
+			$bd=new SQLite3("preguntasyrespuestas.db");
+			$resultado = $bd->query("SELECT * FROM 'cat_piw' ORDER BY 'id' DESC;");
+			while($fila = $resultado->fetchArray()){
+				echo "<tr><td>" .$fila["pregunta"]."</td><td>".$fila["respuesta"]."</td></tr>";
+			}
+			$bd->close();
+		?>
+	</table>
+</body>
+</html>
