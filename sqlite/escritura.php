4eeb28f0d66b7849390cfda14d439c3887770d6c
fizzvr <fizzvr@gmail.com>
2013-08-17 10:17:14 -0500

    insertando datos a sqlite3 con exec

--- sqlite/escritura.php
+++ sqlite/escritura.php
@@ -1,5 +1,5 @@
 <?php
-
-
-
+	$bd = new SQLite3("preguntasyrespuestas.db");
+	$bd->exec("INSERT INTO cat_piw('utc','pregunta','respuesta','ip') VALUES ('t','¿Cuál es la capital de PERÚ?','LIMA','127.1.1.1');");
+	$bd->close();
 ?>
