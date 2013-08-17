dce29654489bd970e7a29dfb90521e52041a0a82
fizzvr <fizzvr@gmail.com>
2013-08-17 13:42:28 -0500

    actulizon con condicion WHERE

--- sqlite/actualizar.php
+++ sqlite/actualizar.php
@@ -1,5 +1,5 @@
 <?php
 	$bd = new SQLite3("preguntasyrespuestas.db");
-	$bd->exec("update cat_piw set ip='localhost'; ");
+	$bd->exec("update cat_piw set ip='127.0.0.1' where id=2; ");
 	$bd->close();
 ?>
