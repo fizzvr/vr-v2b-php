29d14b188733dbf08f80d71279c9c698f807dc83
fizzvr <fizzvr@gmail.com>
2013-08-17 14:25:32 -0500

    añadiendo imagenes a las tablas mediante metodo LOCAL sin BD

--- sqlite/leer.php
+++ sqlite/leer.php
@@ -4,13 +4,13 @@
 	<meta charset="UTF-8">
 	<title>SQLite 3</title>
 </head>
-<body style="font-size: 2em;">
+<body style="font-size: 1em;">
 	<table border=10>
 		<?php
 			$bd=new SQLite3("preguntasyrespuestas.db");
 			$resultado = $bd->query("SELECT * FROM 'cat_piw' ORDER BY 'id' DESC;");
 			while($fila = $resultado->fetchArray()){
-				echo "<tr><td>" .$fila["pregunta"]."</td><td>".$fila["respuesta"]."</td></tr>";
+				echo "<tr><td>".$fila["pregunta"]."</td><td>".$fila["respuesta"]."</td><td><img src='imagenes/".$fila["respuesta"].".jpg' width=300px></td></tr>";
 			}
 			$bd->close();
 		?>
